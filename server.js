5f3cfbdc7b8688f2570c4175bdefe08a5974cf28
Ofek <ofek.howard@gmail.com>
2026-05-24 20:36:23 +0300

    Fix message controller import path

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ import connectDB from './config/db.js'
 import authRoutes from './routes/auth.route.js'
 import chatRoutes from './routes/chat.route.js'
 import userRoutes from './routes/user.route.js'
+import messageRoutes from './routes/message.route.js'
 import initSocket from './sockets/socket.js';
 
 dotenv.config();
@@ -23,6 +24,7 @@ app.use(cors());
 app.use("/api/auth", authRoutes);
 app.use("/api/chat" ,chatRoutes);
 app.use("/api/user", userRoutes);
+app.use("/api/message", messageRoutes)
 
 
 
